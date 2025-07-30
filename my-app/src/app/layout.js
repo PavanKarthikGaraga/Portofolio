d77e6b7da96c73061dca4a3b2777a6f952cb51cf
PavanKarthikGaraga <pavankarthik107@gmail.com>
2025-07-30 16:38:41 +0530

    feat: intial verdion

---
+++ my-app/src/app/layout.js
@@ -0,0 +1,25 @@
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+});
+
+export const metadata = {
+  title: "Garaga Pavan Karthik - Portfolio",
+  description: "Portfolio of Garaga Pavan Karthik",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${poppins.variable} antialiased font-poppins`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
